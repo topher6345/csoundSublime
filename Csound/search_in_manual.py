b5715a62912d6a91266261b614160ffce69cfd0c
Christopher Saunders <topher6345@gmail.com>
2016-09-10 13:02:43 -0700

    Update Csound Manual link.
    Visiting the url without the trailing `.html` filepath was leading to a 404 page.

--- Csound/search_in_manual.py
+++ Csound/search_in_manual.py
@@ -5,7 +5,7 @@ class SearchInManualCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         word = self.view.substr(self.view.word((self.view.sel()[0].b)))
-        webbrowser.open_new_tab("http://www.csounds.com/manual/html/%s" % word)
+        webbrowser.open_new_tab("http://www.csounds.com/manual/html/%s.html" % word)
 
     def is_visible(self):
         return self.view.file_name() and self.view.file_name()[-4:] == ".csd"
